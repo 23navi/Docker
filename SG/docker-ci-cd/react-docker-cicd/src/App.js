68d65632cb0b324053bf54727ca1e73787990862
Navi sureka <navisureka23@gmail.com>
2023-04-21 23:07:29 +0530

    update

--- SG/docker-ci-cd/react-docker-cicd/src/App.js
+++ SG/docker-ci-cd/react-docker-cicd/src/App.js
@@ -8,6 +8,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+        </p>
+        <p>Hello this is navi sureka
+          
         </p>
         <a
           className="App-link"
